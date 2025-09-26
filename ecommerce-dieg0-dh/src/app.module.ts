226e19fb8a63cc54799bbb655dc5b7a433c66c84
Diego <ecua_soul@yahoo.com>
2025-09-25 17:56:58 -0800

    chore: initialize file

---
+++ ecommerce-dieg0-dh/src/app.module.ts
@@ -0,0 +1,42 @@
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import typeorm from './config/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrdersModule } from './orders/orders.module';
+import { CategoriesModule } from './categories/categories.module';
+import { FileUploadModule } from './file-upload/file-upload.module';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    UsersModule,
+    ProductsModule,
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true, load: [typeorm] }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => config.get('typeorm')!,
+    }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.API_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+    OrdersModule,
+    CategoriesModule,
+    FileUploadModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService, LoggerMiddleware],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
