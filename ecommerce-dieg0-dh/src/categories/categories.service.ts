a202fc3e3bf45afe50c859899e6d750d8ecb8085
Diego <ecua_soul@yahoo.com>
2025-09-25 17:57:32 -0800

    chore: initialize file

---
+++ ecommerce-dieg0-dh/src/categories/categories.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import * as data from '../data.json';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Categories } from './entities/category.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Categories)
+    private readonly categoriesRepository: Repository<Categories>,
+  ) {}
+
+  async create(): Promise<string> {
+    const categoriesNames = new Set(data.map((element) => element.category));
+    const categoriesArray = Array.from(categoriesNames);
+    const categories = categoriesArray.map((category) => ({
+      name: category,
+    }));
+
+    await this.categoriesRepository
+      .createQueryBuilder()
+      .insert()
+      .into(Categories)
+      .values(categories)
+      .orIgnore()
+      .execute();
+
+    return 'Categories created';
+  }
+
+  findAll() {
+    return this.categoriesRepository.find();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} category`;
+  }
+
+  update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    return `This action updates a #${id} category`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} category`;
+  }
+}
