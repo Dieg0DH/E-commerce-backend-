cb94e8dfaaf3256c7e4af75eab47ad6c21451411
Diego <ecua_soul@yahoo.com>
2025-09-25 17:55:58 -0800

    chore: initialize file

---
+++ ecommerce-dieg0-dh/dist/products/entities/product.entity.d.ts
@@ -0,0 +1,12 @@
+import { Categories } from 'src/categories/entities/category.entity';
+import { OrderDetails } from 'src/orders/entities/order.Details.entity';
+export declare class Products {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    stock: number;
+    imgUrl: string;
+    category: Categories;
+    orderDetails: OrderDetails[];
+}
