3db00a6d8df23e22f80f7c98e1d1024948c36712
Diego <ecua_soul@yahoo.com>
2025-09-25 17:57:15 -0800

    chore: initialize file

---
+++ ecommerce-dieg0-dh/src/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from 'src/users/enities/users.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Users])],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
