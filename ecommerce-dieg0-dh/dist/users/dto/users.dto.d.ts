1c3e4fd6da95660e088cefd166b5c57337e69503
Diego <ecua_soul@yahoo.com>
2025-09-25 17:56:36 -0800

    chore: initialize file

---
+++ ecommerce-dieg0-dh/dist/users/dto/users.dto.d.ts
@@ -0,0 +1,17 @@
+export declare class CreateUserDto {
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+    address: string;
+    phone: number;
+    city: string;
+    country: string;
+}
+declare const LoginUserDto_base: import("@nestjs/common").Type<Pick<CreateUserDto, "email" | "password">>;
+export declare class LoginUserDto extends LoginUserDto_base {
+}
+declare const UpdateUserDto_base: import("@nestjs/common").Type<Partial<CreateUserDto>>;
+export declare class UpdateUserDto extends UpdateUserDto_base {
+}
+export {};
