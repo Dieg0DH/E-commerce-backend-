23afa53c8a1987cdccbd76544411b056755c6000
Diego <ecua_soul@yahoo.com>
2025-09-25 17:58:43 -0800

    chore: initialize file

---
+++ ecommerce-dieg0-dh/src/products/products.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { ProductsController } from './products.controller';
+import { ProductsRepository } from './products.Repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categories } from 'src/categories/entities/category.entity';
+import { Products } from './entities/product.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Categories, Products])],
+  controllers: [ProductsController],
+  providers: [ProductsService, ProductsRepository],
+})
+export class ProductsModule {}
