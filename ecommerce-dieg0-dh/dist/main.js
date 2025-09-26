13df6b858ab5841660d4226782320ba1eaa3cd12
Diego <ecua_soul@yahoo.com>
2025-09-25 17:44:29 -0800

    chore: initialize file

---
+++ ecommerce-dieg0-dh/dist/main.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const common_1 = require("@nestjs/common");
+const swagger_1 = require("@nestjs/swagger");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const swaggerConfig = new swagger_1.DocumentBuilder()
+        .setTitle('Ecommerce API')
+        .setDescription('Ecommerce API description')
+        .setVersion('1.0.0')
+        .setDescription('Esta es la documentacion de la API de ecommerce M4')
+        .addBearerAuth()
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, swaggerConfig);
+    swagger_1.SwaggerModule.setup('api', app, document);
+    app.useGlobalPipes(new common_1.ValidationPipe());
+    await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
